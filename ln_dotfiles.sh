c65c605de91b42db1f96d799cb944253dab981a1
yshira <yuta1125tp@gmail.com>
2020-03-27 13:22:50 +0900

    fix dirname depth

--- ln_dotfiles.sh
+++ ln_dotfiles.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-script=$(readlink -f "$0")
-basedir=$(dirname "$script")
-ln -sf ${basedir}/dotfiles/.vimrc ~/.vimrc
-ln -sf ${basedir}/dotfiles/.tmux.conf ~/.tmux.conf
+dotfiles_root=$(dirname "`readlink -f "$0"`")
+ln -sf ${dotfiles_root}/.vimrc ~/.vimrc
+ln -sf ${dotfiles_root}/.tmux.conf ~/.tmux.conf
